1f3da03a7d71a1d00eae34abf23fe8724e5ad78b
David Valverde Fernández <david.valverde@campusfp.es>
2022-03-23 09:58:47 +0100

    primer commit de ejemplo

---
+++ src/com/ejemplo/teoria/Prueba.java
@@ -0,0 +1,13 @@
+package com.ejemplo.teoria;
+
+public class Prueba {
+	
+	//void no devuelve datos
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("probando proyecto");
+		
+	}// cierra método
+
+}// cierra clase
